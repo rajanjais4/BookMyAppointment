0fb82d223fcdf196db6b13f9ce615e7b5454949b
rajanjais4 <rajanjais4@gmail.com>
2023-04-21 01:26:36 +0530

    Swagger paths allowed

--- src/main/java/com/indra/bookMyAppointment/config/SecurityConfiguration.java
+++ src/main/java/com/indra/bookMyAppointment/config/SecurityConfiguration.java
@@ -24,12 +24,28 @@ public class SecurityConfiguration {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     String permittedString="/auth/**";
+    final String[] AUTH_WHITELIST = {
+
+            // for Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-ui.html",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/webjars/**",
+
+            // for Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+    };
     http
         .csrf()
         .disable()
         .authorizeRequests()
         .antMatchers(permittedString)
           .permitAll()
+            .antMatchers(AUTH_WHITELIST).permitAll()
         .anyRequest()
           .authenticated()
         .and()
